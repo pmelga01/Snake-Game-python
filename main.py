5232c4f0fdc3204ae801c3f4db832c1a6b8d51f6
Pamela Melgar <pamela.melgar@tufts.edu>
2023-12-26 11:07:36 -0500

    First commit snake game

---
+++ main.py
@@ -0,0 +1,12 @@
+# Snake Game
+from turtle import Turtle, Screen
+
+tim = Turtle()
+screen = Screen()
+
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+
+
+screen.exitonclick()
